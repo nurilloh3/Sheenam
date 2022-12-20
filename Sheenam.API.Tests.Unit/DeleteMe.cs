1ba56dd20ac5450e23c7acf939320cc35a031a8e
Nurilloh <nurilloh6@gmail.com>
2022-12-21 04:22:38 +0500

    INFRA: Initialize Unit Test Project

---
+++ Sheenam.API.Tests.Unit/DeleteMe.cs
@@ -0,0 +1,14 @@
+﻿//===============================================
+//Copyrigth(c) Coalitionof Good-Hearted Engineers
+//Free To Use Comfort And Peace
+//===============================================
+
+namespace Sheenam.API.Tests.Unit
+{
+	public class DeleteMe
+	{
+		[Fact]
+		public void ShouldBeTrue() => Assert.True(true);
+	}
+}
+
